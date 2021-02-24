7f144f30b36770be398be91775ec2d2086ba1f83
haodi <changgaoyou@163.com>
2021-02-24 18:01:22 +0800

    my first commit --flink

---
+++ flink-kafka/src/main/java/com/atguigu/practice/SourceTest1_Collection.java
@@ -0,0 +1,36 @@
+package com.atguigu.practice;
+
+import com.atguigu.bean.SensorReading;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 从集合中读取数据
+ */
+public class SourceTest1_Collection {
+    public static void main(String[] args) throws Exception {
+        //创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+
+        //c从集合中读取数据
+        DataStreamSource<SensorReading> sensorReadingDataStreamSource = env.fromCollection(
+                Arrays.asList(
+                        new SensorReading("sensor_1", 1547718199L, 35.8),
+                        new SensorReading("sensor_6", 1547718201L, 15.4),
+                        new SensorReading("sensor_7", 1547718202L, 6.7),
+                        new SensorReading("sensor_10", 1547718205L, 38.1)
+
+                )
+        );
+
+        sensorReadingDataStreamSource.print();
+
+        //3.执行
+        env.execute();
+    }
+
+}
